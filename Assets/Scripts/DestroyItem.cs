3c20a250963864acf154606f68bef6f54637f0d5
DESKTOP-4VEP19R\VAPORHEAD <kapstoneboyz@gmail.com>
2024-12-10 11:15:34 +0800

    .

--- Assets/Scripts/DestroyItem.cs
+++ Assets/Scripts/DestroyItem.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class DestroyItem : MonoBehaviour
 {
     public PickUp item;
     public PickUpGamePlay gamePlay;
-    public TMP_Text blanket;
+    
 
     public void PickUpObject()
     {
         gamePlay.PickUpItem();
         Destroy(item.gameObject);
     }
-    public void ChangeTextColor()
-    {
-        blanket.color = Color.green;
-    }
+    
 
 
 }
